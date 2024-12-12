7b6d48e6ff7f24dd35f773d0b5b01d476e094794
emina.mahmutbegovic <eminamahmutbey@gmail.com>
2024-12-12 19:12:54 +0000

    chore: modify motion generator

--- src/ur5_state_publisher/include/joint_motion_generator.hpp
+++ src/ur5_state_publisher/include/joint_motion_generator.hpp
@@ -1,3 +1,13 @@
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/frames.hpp>
+#include <kdl/jacobian.hpp>
+#include <kdl/jntarray.hpp>
+
 namespace ur5 {
 
 class JointMotionGenerator {
